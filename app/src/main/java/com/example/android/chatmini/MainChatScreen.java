7806e1ed35d075e4b29d025e1b0d7be08d93d892
kinginthenorth <khan019950@gmail.com>
2018-05-09 17:47:49 +0530

    firebase added

---
+++ app/src/main/java/com/example/android/chatmini/MainChatScreen.java
@@ -0,0 +1,24 @@
+package com.example.android.chatmini;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+
+public class MainChatScreen extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_chat_screen);
+
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return  super.onCreateOptionsMenu(menu);
+    }
+}
