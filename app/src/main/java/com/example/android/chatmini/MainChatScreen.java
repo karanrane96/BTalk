d33247c27af0e178d187fae0ae853a91b63b1d43
kinginthenorth <khan019950@gmail.com>
2018-05-11 23:29:40 +0530

    firebase added

--- app/src/main/java/com/example/android/chatmini/MainChatScreen.java
+++ app/src/main/java/com/example/android/chatmini/MainChatScreen.java
@@ -1,24 +1,85 @@
 package com.example.android.chatmini;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
-
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Toast;
 
 public class MainChatScreen extends AppCompatActivity {
-
+    FloatingActionButton mainFab, addPeople, peopleList;
+    Animation fabOpen, fabClose, rotateClockwise, rotateAntiClockwise;
+    boolean isOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_chat_screen);
-
+        mainFab = findViewById(R.id.floatBtn);
+        addPeople = findViewById(R.id.float_add_ppl);
+        peopleList = findViewById(R.id.float_ppl_list);
+        fabOpen = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_open);
+        fabClose = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fab_close);
+        rotateClockwise = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_clockwise);
+        rotateAntiClockwise = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_anticlockwise);
 
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem){
+
+        switch (menuItem.getItemId()) {
+            case R.id.reqStatus:
+                Toast.makeText(this,"req",Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.search:
+                Toast.makeText(this,"search",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.menu_profile:
+                Toast.makeText(this,"profile",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.menu_req_st:
+                Toast.makeText(this,"req in menu",Toast.LENGTH_LONG).show();
+                break;
+
+            default:
+                Toast.makeText(this,"hello",Toast.LENGTH_LONG).show();
+
+        }
+
+        return super.onOptionsItemSelected(menuItem);
+    }
+
+    public void onFabClick(View view) {
+        Toast.makeText(this, "mainFab clicked", Toast.LENGTH_SHORT).show();
+        if(isOpen) {
+            addPeople.startAnimation(fabClose);
+            peopleList.startAnimation(fabClose);
+            mainFab.startAnimation(rotateAntiClockwise);
+            addPeople.setClickable(false);
+            peopleList.setClickable(false);
+            isOpen = false;
+        } else {
+            addPeople.startAnimation(fabOpen);
+            peopleList.startAnimation(fabOpen);
+            mainFab.startAnimation(rotateClockwise);
+            addPeople.setClickable(true);
+            peopleList.setClickable(true);
+            isOpen = true;
+        }
+    }
+
 }
--- app/src/main/java/com/example/android/chatmini/SplashScreen.java
+++ app/src/main/java/com/example/android/chatmini/SplashScreen.java
@@ -1,13 +1,29 @@
 package com.example.android.chatmini;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class SplashScreen extends AppCompatActivity {
 
+    Button loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainChatScreen.class);
+                intent.addFlags(intent.FLAG_ACTIVITY_NEW_TASK | intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
     }
+
 }
