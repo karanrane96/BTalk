8a72f98dc05c1789e00a0e752a082f27aeb8e11b
Karan Rane <karanrane96@gmail.com>
2018-05-15 03:30:14 +0530

    sleeping

--- app/src/main/java/com/example/android/chatmini/UserChat.java
+++ app/src/main/java/com/example/android/chatmini/UserChat.java
@@ -10,7 +10,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
